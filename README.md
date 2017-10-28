9d763343f7a171ab840f29b5d88b16141c5eba6a
heathermoore <moore.hj@gmail.com>
2017-10-27 20:10:16 -0400

    radiating lines

---
+++ README.md
@@ -0,0 +1,7 @@
+#Ferro
+
+###Dev
+`npm install http-server`
+`http-server`
+
+Sound demos will be at localhost:8080
--- sketch.js
+++ sketch.js
@@ -5,33 +5,53 @@ function preload(){
 function setup(){
   var cnv = createCanvas(600,600);
   cnv.mouseClicked(togglePlay);
-  fft = new p5.FFT(.95,16);
+  fft = new p5.FFT(.95,1024);
   sound.amp(0.2);
+}
+
+function drawWaveform() {
+  var waveform = fft.waveform();
+  noFill();
+  beginShape();
+  stroke(255, 0, 0); // waveform is red
+  strokeWeight(1);
+  for (var i = 0; i < waveform.length; i++) {
+    var x = map(i, 0, waveform.length, 0, width);
+    var y = map(waveform[i], -1, 1, 0, height);
+    vertex(x, y);
+  }
+  endShape();
+}
+function drawSpectrum() {
+  var spectrum = fft.analyze();
+  noStroke();
+  fill(0, 255, 0); // spectrum is green
+  for (var i = 0; i < spectrum.length; i++) {
+    var x = map(i, 0, spectrum.length, 0, width);
+    var h = -height + map(spectrum[i], 0, 255, height, 0);
+    rect(x, height, width / spectrum.length, h)
+  }
+}
+
+function drawSpectrumAsCircle() {
+  var spectrum = fft.analyze();
+  stroke(0, 0, 255);
+  strokeWeight(1);
+  for (var i = 0; i < spectrum.length; i++) {
+    var h = -height + map(spectrum[i], 0, 255, height, 0);
+
+    var radians=i*Math.PI/180*2;
+    var outerX = 300 + h * Math.cos(radians);
+    var outerY = 300 + h * Math.sin(radians);
+
+    line(300, 300, outerX, outerY);
+  }
 }
 
 function draw(){
   background(0);
-
-  var spectrum = fft.analyze();
-  noStroke();
-  fill(0,255,0); // spectrum is green
-  for (var i = 0; i< spectrum.length; i++){
-    var x = map(i, 0, spectrum.length, 0, width);
-    var h = -height + map(spectrum[i], 0, 255, height, 0);
-    rect(x, height, width / spectrum.length, h )
-  }
-
-  var waveform = fft.waveform();
-  noFill();
-  beginShape();
-  stroke(255,0,0); // waveform is red
-  strokeWeight(1);
-  for (var i = 0; i< waveform.length; i++){
-    var x = map(i, 0, waveform.length, 0, width);
-    var y = map( waveform[i], -1, 1, 0, height);
-    vertex(x,y);
-  }
-  endShape();
+  drawWaveform();
+  drawSpectrumAsCircle();
 }
 
 // fade sound if mouse is over canvas

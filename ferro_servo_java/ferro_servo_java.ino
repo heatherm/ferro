87e1e4a75496f82f96deb4b83c3fed1a7ce2f9ba
heathermoore <moore.hj@gmail.com>
2017-11-07 20:00:09 -0500

    blind refactoring

--- ferro_servo_java/ferro_servo_java.ino
+++ ferro_servo_java/ferro_servo_java.ino
@@ -2,12 +2,13 @@
 
 #define MAX_MILLIS_TO_WAIT 100
 
-Servo myservo;
+Servo servo;
+int servoStopSpeed = 90;
+
 int input;
 int button1 = 4;
 int button2 = 5;
 
-int servoSpeed = 90;
 int pot1 = A1;
 int lastPot1Value;
 
@@ -17,74 +18,97 @@ byte outgoing[2];
 byte incoming[2];
 
 void setup() {
-  Serial.end();
-  myservo.attach(13);
-  Serial.begin(9600);
+  setupSerial(9600);
+  setupServo(13);
+  setupButton(button1);
+  setupButton(button2);
+  
+  lastPot1Value = analogRead(A1);
+}
 
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+void loop() {
+  receiveTwoBytes();
+    if (shouldStopServo()) {
+      stopServo();
+    else if (receivedServoDirection()) {
+      servo.write(incoming[1]);
+    }
   }
+
+  handleButtonClick(button1, 236);
+  handleButtonClick(button2, 237);
+
+  handlePotentiometerTurn(238);
+}
+
+void setupSerial(int baud) {
+  Serial.end();
+  Serial.begin(baud);
+  while (!Serial) {};
 
   for (int i=0; i<2; i++) {
     outgoing[i] = byte(0);
     incoming[i] = byte(0);
   }
-  
-  pinMode(button1, INPUT);
-  pinMode(button2, INPUT);
-
-  digitalWrite(button1, LOW);
-  digitalWrite(button2, LOW);
-  
-  myservo.write(servoSpeed);
-  lastPot1Value = analogRead(A1);
 }
 
-void loop() {
-  // receive 2 bytes
+void setupServo(int servoPin) {
+  myservo.attach(13);
+  myservo.write(servoStopSpeed);
+}
+
+void setupButton(int buttonPin){
+  pinMode(buttonPin, INPUT);
+  digitalWrite(buttonPin, LOW);
+}
+
+void receiveTwoBytes() {
   starttime = millis();
   while ( (Serial.available() < 2) && ((millis() - starttime) < MAX_MILLIS_TO_WAIT) ) {
-    // hang in this loop until we either get 2 bytes of data or 1 second
-    // has gone by
+    //wait for bytes
   }
   if (Serial.available() < 2) {
-    // the data didn't come in - handle that problem here
+    // the data didn't come in - flush the port
     Serial.flush();
   } else {
-    for(int n=0; n < 2; n++) {
-      incoming[n] = Serial.read(); // Then: Get them.
+    for(int i=0; i<2; i++) {
+      incoming[i] = Serial.read();
     }
-    if (incoming[0] == 227) {
-      myservo.write(servoSpeed);
-    }
-    else if (incoming[0] == 226) {
-      myservo.write(incoming[1]);
-    }
-  }
-  
-  // send button presses  
-  if (digitalRead(button1) == HIGH) {
-    outgoing[0] = byte(236);
-    Serial.write(outgoing, sizeof(outgoing));
-    delay(300);
-  } 
-//  else if (digitalRead(button2) == HIGH) {
-//    outgoing[0] = byte(237);
-//    Serial.write(outgoing, sizeof(outgoing));
-//    delay(300);
-//  }
-
-  //  check for and send any potentiometer1 changes
-  int currentRead = analogRead(A1);
-  bool thing = abs(currentRead - lastPot1Value) > 100;
-  if (thing > 0) {
-    lastPot1Value = currentRead;
-    outgoing[0] = byte(238);
-//    outgoing[1] = byte(map(lastPot1Value, 0, 1023, 5, 225));
-    outgoing[1] = byte(map(lastPot1Value, 0, 1023, 0, 9));
-
-    Serial.write(outgoing, sizeof(outgoing));    
-    delay(300);
   }
 }
 
+void writeOutgoing() {
+  Serial.write(outgoing, sizeof(outgoing));    
+  delay(300);
+}
+
+boolean receivedServoDirection() {
+  return incoming[0] == 226;
+}
+
+boolean shouldStopServo() {
+ return incoming[0] == 227;
+}
+
+void stopServo() {
+  servo.write(servoStopSpeed);
+}
+
+void handleButtonClick(int buttonPin, int buttonUniqueId) {
+  if (digitalRead(buttonPin) == HIGH) {
+    outgoing[0] = byte(buttonUniqueId);
+    writeOutgoing();
+  }  
+}
+
+void handlePotentiometerTurn(int uniqueId) {
+  int currentRead = analogRead(A1);
+  bool pastThreshold = abs(currentRead - lastPot1Value) > 100;
+  if (pastThreshold == true) {
+    lastPot1Value = currentRead;
+    outgoing[0] = byte(uniqueId);
+//    outgoing[1] = byte(map(lastPot1Value, 0, 1023, 5, 225));
+    outgoing[1] = byte(map(lastPot1Value, 0, 1023, 0, 9));
+    writeOutgoing();
+   }
+}
